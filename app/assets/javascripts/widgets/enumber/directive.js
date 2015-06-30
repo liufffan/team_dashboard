64519208ef7ffc02d9c2458acfab2102f3942840
Gen1us2k <minkin.andrew@gmail.com>
2015-06-30 15:20:28 +0600

    Support for reversed metrics

--- app/assets/javascripts/widgets/enumber/directive.js
+++ app/assets/javascripts/widgets/enumber/directive.js
@@ -1,37 +1,45 @@
-app.directive("enumber", ["ENumberModel", "SuffixFormatter", function(ENumberModel, SuffixFormatter) {
+app.directive("enumber", ["ENumberModel", "SuffixFormatter", function (ENumberModel, SuffixFormatter) {
 
-  var linkFn = function(scope, element, attrs) {
-    // console.log(element.html(), attrs)
+    var linkFn = function (scope, element, attrs) {
+        // console.log(element.html(), attrs)
 
-    function calculatePercentage(value, previousValue) {
-      console.log("previous", previousValue, "value", value);
-      if(previousValue == 0) {
-        return 0;
-      }
-      return ((value - previousValue) / previousValue) * 100;
-    }
+        function calculatePercentage(value, previousValue) {
+            console.log("previous", previousValue, "value", value);
+            if (previousValue == 0) {
+                return 0;
+            }
+            return ((value - previousValue) / previousValue) * 100;
+        }
 
-    function onSuccess(data) {
-      scope.data = data;
-      scope.data.label = scope.data.label || scope.widget.label;
-      if(scope.data.value > 10) { scope.data.value = Math.round(scope.data.value); } // We want to see fractions for small numbers
-      scope.data.stringValue = scope.widget.use_metric_suffix ? SuffixFormatter.format(scope.data.value, 1) : scope.data.value.toString();
+        function onSuccess(data) {
+            scope.data = data;
+            scope.data.label = scope.data.label || scope.widget.label;
+            if (scope.data.value > 10) {
+                scope.data.value = Math.round(scope.data.value);
+            } // We want to see fractions for small numbers
+            scope.data.stringValue = scope.widget.use_metric_suffix ? SuffixFormatter.format(scope.data.value, 1) : scope.data.value.toString();
 
-      scope.data.secondaryValue = calculatePercentage(scope.data.value, scope.data.prev_value);
-      scope.data.arrow = scope.data.secondaryValue > 0 ? "arrow-up" : "arrow-down";
-      scope.data.color = scope.data.secondaryValue > 0 ? "color-up" : "color-down";
-    }
+            scope.data.secondaryValue = calculatePercentage(scope.data.value, scope.data.prev_value);
+            if (scope.widget.reversed_metric ){
+                scope.data.arrow = scope.data.secondaryValue < 0 ? "arrow-up" : "arrow-down";
+                scope.data.color = scope.data.secondaryValue < 0 ? "color-up" : "color-down";
+            } else {
+                scope.data.arrow = scope.data.secondaryValue > 0 ? "arrow-up" : "arrow-down";
+                scope.data.color = scope.data.secondaryValue > 0 ?"color-up" : "color-down";
+            }
 
-    function update() {
-      return ENumberModel.getData(scope.widget).success(onSuccess);
-    }
+        }
 
-    scope.init(update);
-      console.log("FUUU;");
-  };
+        function update() {
+            return ENumberModel.getData(scope.widget).success(onSuccess);
+        }
 
-  return {
-    templateUrl: "templates/widgets/enumber/show.html",
-    link: linkFn
-  };
+        scope.init(update);
+        console.log("FUUU;");
+    };
+
+    return {
+        templateUrl: "templates/widgets/enumber/show.html",
+        link: linkFn
+    };
 }]);
